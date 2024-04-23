56b0b6ce0d75b0f9a5030d6f623b8dbbc5fa0743
McDadyh <james.fierens@outlook.fr>
2024-04-23 15:17:37 +0200

    display response weather

--- assets/main.js
+++ assets/main.js
@@ -8,17 +8,35 @@ document.getElementById("weather").addEventListener("click", function (e) {
     let url = baseUrl + "&q=" + city + "&aqi=no";
 
     fetch(url)
-        .then((response) => {
+      .then((response) => {
         if (!response.ok) {
-            throw new Error("Network response was not ok");
+          throw new Error("Network response was not ok");
         }
         return response.json();
-        })
-        .then((data) => {
-        console.log("success", data);
-        })
-        .catch((error) => {
-        console.error("fail", error);
-        });
+      })
+      .then((data) => {
+        const city = data.location.name;
+        const country = data.location.country;
+        const region = data.location.region;
+        const condition = data.current.condition.text;
+        const icon = data.current.condition.icon;
+        const temperature = data.current.temp_c;
+        const windSpeed = data.current.wind_kph;
+
+        // Affichage des données dans le DOM
+        document.getElementById("cityWeather").textContent = "Ville : " + city;
+        document.getElementById("country").textContent = "Pays : " + country;
+        document.getElementById("region").textContent = "Région : " + region;
+        document.getElementById("condition").textContent = "Conditions : " + condition;
+        document.getElementById("icon").src = "https:" + icon;
+        document.getElementById("temperature").textContent = "Température : " + temperature + "°C";
+        document.getElementById("windSpeed").textContent = "Vitesse du vent : " + windSpeed + " km/h";
+      })
+      .catch((error) => {
+        console.error(
+          "Erreur lors de la récupération des données météorologiques :",
+          error
+        );
+      });
 });
 
